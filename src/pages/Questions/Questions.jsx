fce1f6b3891e6b22b9dfd576cb69f60b6dad2a9d
nuguntu <nskhiereli@gmail.com>
2023-04-13 23:49:25 +0400

    fix: answers

--- src/pages/Questions/Questions.jsx
+++ src/pages/Questions/Questions.jsx
@@ -7,28 +7,24 @@ function Questions() {
   const params = useParams();
   const [questions, setQuestions] = useState([]);
   const [current, setCurrent] = useState([]);
+  const [answers, setAnswers] = useState([]);
   const [rendered, setRendered] = useState(false);
-  console.log(current);
-  console.log(questions);
-  if (current?.answers) console.log(current?.answers?.replace(/'/g, '"'));
-  if (current?.answers) console.log(current?.answers);
 
   useEffect(() => {
     async function fetch() {
       const res = await BasicAxios.get("tests/" + params.id);
       setQuestions(res.data);
       setCurrent(res.data.find((question) => question.id == params.questionId));
+      const answersParse = JSON.parse(
+        res.data.find((question) => question.id == params.questionId).answers
+      );
+      setAnswers(answersParse);
       setRendered(true);
     }
     fetch();
   }, []);
 
-  console.log(current);
-  console.log(questions);
-  console.log(current?.answers?.replace(/'/g, '"')["a"]);
-  if (current?.answers != undefined) console.log(current?.answers["a"]);
-
-  if (!questions || !current || !rendered) return;
+  if (!questions || !current || !rendered || !answers) return;
   return (
     <section className="w-[100vw] min-h-[100vh]">
       <div className="pt-[50px] max-w-[80%] m-auto min-h-[100vh]">
@@ -45,17 +41,17 @@ function Questions() {
           })}
         </ul>
         <p className="text-[16px] italic mt-[30px]">{current.question}</p>
-        <div className="bg-red-400">
-          {/* {Object.keys(JSON.parse(current.answers.replace(/'/g, '"'))).map(
-            (key, i) => {
-              return (
-                <p key={i} className="text-[16px] mt-[20px]">
-                  {current.answers[key]}
-                </p>
-              );
-              // <BasicCheckbox></BasicCheckbox>;
-            }
-          )} */}
+        <div className="mt-[30px] flex flex-col items-start justify-center gap-[7px]">
+          {Object.keys(answers).map((key) => (
+            <div
+              key={key}
+              className="text-[16px] flex items-center justify-start gap-[10px]"
+            >
+              <p>{key})</p>
+              <p>{answers[key]}</p>
+            </div>
+          ))}
+          {/* // <BasicCheckbox></BasicCheckbox>; */}
           {/* <p className="text-[16px] mt-[20px]">{current.answers["ა"]}</p> */}
         </div>
       </div>
