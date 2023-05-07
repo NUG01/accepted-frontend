6b2fc75137ee9c8014864aeb967cfb26840ea274
nuguntu <nskhiereli@gmail.com>
2023-05-07 22:59:38 +0400

    style: comments

--- src/pages/Corridor/components/PostFooter.jsx
+++ src/pages/Corridor/components/PostFooter.jsx
@@ -1,19 +1,39 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import RockAndRoll from "../../../assets/icons/RockAndRoll";
 import CommentIcon from "../../../assets/icons/CommentIcon";
 import styles from "../Corridor.module.scss";
 import CloseCircle from "../../../assets/icons/CloseCircle";
+import WriteCommentIcon from "../../../assets/icons/WriteCommentIcon";
 
 function PostFooter({ data, liked, likeHandler, likeLength }) {
   const [likeListModalShow, setLikeListModalShow] = useState(false);
+  const [comment, setComment] = useState("");
+  const commentRef = useRef();
+
+  // commentRef?.current?.addEventListener("keydown", (e) => {
+  //   if (e.key == "Enter") {
+  //     e.preventDefault();
+  //     console.log(comment);
+  //   }
+  // });
+
+  function commentFormHandler(ev) {
+    if (ev.key == "Enter") {
+      ev.preventDefault();
+      if (!comment) return;
+
+      console.log(comment);
+    }
+  }
   return (
     <div className="self-start flex flex-col items-center justify-start  w-[100%] px-[12px] mt-[-7px]">
-      {likeListModalShow && (
+      {likeListModalShow && likeLength > 0 && (
         <div className={styles.addQuestionModal}>
           <div
             onClick={() => setLikeListModalShow(false)}
             className={`${styles.backdrop}`}
           ></div>
+
           <div className={`${styles.likesModal}`}>
             <div className={styles.modalHeader}>
               <p className="py-[5px] text-[20px] text-[var(--extra-light-normal-beige)]">
@@ -41,13 +61,15 @@ function PostFooter({ data, liked, likeHandler, likeLength }) {
       <div className="w-[100%] px-[5px] flex items-center justify-start  border-b-[1px] border-b-[var(--main-gray)]">
         <div
           onClick={() => setLikeListModalShow(true)}
-          className="scale-[0.7] flex items-center justify-center text-[var(--dark-gray)] text-[21px] cursor-pointer"
+          className={`scale-[0.7] flex items-center justify-center text-[var(--dark-gray)] text-[21px] ${
+            likeLength > 0 ? "cursor-pointer" : undefined
+          }`}
         >
           <RockAndRoll />
           <p>{likeLength}</p>
         </div>
       </div>
-      <div className="text-[var(--dark-gray)] flex items-center justify-start pt-[3px] gap-[18px] w-[100%]">
+      <div className="text-[var(--dark-gray)] flex items-center justify-start pt-[7px] gap-[18px] w-[100%]">
         <div
           onClick={likeHandler}
           className={` flex items-center justify-center cursor-pointer rounded-[5px] transition-all hover:bg-[var(--extra-light-ocean-blue)] px-[8px] py-[5px] `}
@@ -58,6 +80,25 @@ function PostFooter({ data, liked, likeHandler, likeLength }) {
         <div className="flex items-center justify-center cursor-pointer rounded-[5px] transition-all hover:bg-[var(--extra-light-ocean-blue)] px-[8px] py-[5px]">
           <CommentIcon />
           <p className="font-[600]">კომენტირება</p>
+        </div>
+      </div>
+      <div className="w-[100%] border-t-[1px] border-t-[var(--main-gray)] mt-[8px]">
+        <div className="relative">
+          <textarea
+            onKeyDown={(ev) => commentFormHandler(ev)}
+            className={styles.textarea}
+            name="comment"
+            id="comment"
+            placeholder="კომენტარის დაწერა..."
+            value={comment}
+            onChange={(ev) => setComment(ev.target.value)}
+          ></textarea>
+          {/* <button
+            type="submit"
+            className="absolute bottom-0 right-0 z-50 cursor-pointer"
+          >
+            <WriteCommentIcon></WriteCommentIcon>
+          </button> */}
         </div>
       </div>
     </div>
