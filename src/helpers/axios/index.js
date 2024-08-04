fa1a6c755bccacb0cb2c1841c1029e16f2f4f61b
NUG01 <100063106+NUG01@users.noreply.github.com>
2024-08-04 16:41:57 +0400

    Update index.js

--- src/helpers/axios/index.js
+++ src/helpers/axios/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 const BasicAxios = axios.create({
   // baseURL: "http://localhost:8000/api/",
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -6,6 +7,7 @@ const BasicAxios = axios.create({
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
+    'X-CSRF-TOKEN': csrfToken,
     // "Access-Control-Allow-Origin": "*",
   },
 });
